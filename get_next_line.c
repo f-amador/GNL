d06124468653fc6f2940a55d7a4c0779da30b8f6
f-amador <129676398+f-amador@users.noreply.github.com>
2025-06-21 22:23:56 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -18,7 +18,7 @@ char	*get_next_line(int fd)
 	static char	buff[BUFFER_SIZE + 1];
 
 	str = NULL;
-	if (fd < 0 || BUFFER_SIZE < 1)
+	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, buff, 0) < 0)
 	{
 		if (BUFFER_SIZE > 0)
 		{
